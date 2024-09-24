eb3e376eb31baf0d8625a4bd59e4f95f32ecaacf
taehwan <taehwan4072@naver.com>
2024-09-24 16:48:01 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,5 +12,11 @@ function login(){
         psword: psword.value,
     };
 
-    console.log(req);
+    fetch("/login", {
+        method: "POST",
+        headers:{
+            "Content-Type" : "application/json",
+        }
+        body: JSON.stringify(req),
+    });
 }
