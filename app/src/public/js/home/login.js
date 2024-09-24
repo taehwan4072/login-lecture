a27a34d88b012fc0648874921a346e4a48d307b3
taehwan <taehwan4072@naver.com>
2024-09-24 17:14:28 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -16,7 +16,8 @@ function login(){
         method: "POST",
         headers:{
             "Content-Type" : "application/json",
-        }
+        },
         body: JSON.stringify(req),
-    });
+    }).then((res) => res.json())
+    .then((res) => console.log(res));
 }
