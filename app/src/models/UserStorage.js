e8dfc92185f7ab005ee2eb46a1aa3cfcdadf7a23
taehwan <taehwan4072@naver.com>
2024-09-25 14:02:42 +0900

    user 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -17,6 +17,17 @@ class UserStorage{
         },{});
         return newUsers;
     }
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users);
+        const userInfo = userKeys.reduce((newUsers,info)=>{
+            newUsers[info] = users[info][idx];
+            return newUsers;
+        },{});
+        
+        return userInfo;
+    }
 }
 
 module.exports = UserStorage;
